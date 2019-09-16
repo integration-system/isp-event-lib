db80f8cdce0804224c7f8ef42e7aafaa0838e743
ykomissarov <merser1q@gmail.com>
2019-09-16 16:09:10 +0300

    add util method

---
+++ client/utils.go
@@ -0,0 +1,13 @@
+package client
+
+import (
+	"fmt"
+	"github.com/integration-system/isp-lib/structure"
+	"strings"
+)
+
+func GetId(prefix, moduleName string, outerAddress structure.AddressConfiguration) string {
+	clearAddr := strings.ReplaceAll(outerAddress.GetAddress(), ".", "_")
+	clearAddr = strings.ReplaceAll(clearAddr, ":", "-")
+	return fmt.Sprintf("%s__%s", moduleName, clearAddr)
+}
